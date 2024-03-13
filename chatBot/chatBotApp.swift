ba7ab3095e34a75bb4679453820d5dc56a3dcfb2
miz <mizu710tr@gmail.com>
2024-03-14 00:41:26 +0900

    done

---
+++ chatBot/chatBotApp.swift
@@ -0,0 +1,18 @@
+//
+//  chatBotApp.swift
+//  chatBot
+//  
+//  Created by miztnishi on 2023/12/23
+//  
+//
+
+import SwiftUI
+
+@main
+struct chatBotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
